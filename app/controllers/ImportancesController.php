167771f92859f0722e8001669caa63fa5ff0317b
Kristian Lauttamus <krizzuq@hotmail.com>
2016-02-22 00:33:24 +0200

    Some controllers WIP

---
+++ app/controllers/ImportancesController.php
@@ -0,0 +1,94 @@
+<?php
+
+class ImportancesController extends BaseController
+{
+
+    public static function index()
+    {
+        $importances = Importance::all();
+
+        View::make('importances.html', array('importances' => $importances));
+    }
+
+    public static function create()
+    {
+        View::make('importances-create.html');
+    }
+
+    public static function store()
+    {
+        $params = $_POST;
+
+        $importance = new Importance();
+
+        $errors = $importance->errors();
+        if (count($errors) > 0) {
+            flash()->error(':(', 'Something was a little off...');
+
+            Redirect::to('/importances/create', array('errors' => $errors, 'attributes' => $params));
+        }
+
+        $importance->save();
+
+        foreach($params['role'] as $role){
+            $importanceRole = new ImportanceRole(array(
+                'importance_id' => $importance->id,
+                'role_id' => $role['role_id'],
+                'needed' => $role['needed']
+            ));
+            $importanceRole->save();
+        }
+
+        Redirect::to('/importances#' . $importance->id);
+    }
+
+    public static function edit($id)
+    {
+        $hospital = Hospital::find($id);
+
+        if ($hospital == null) {
+            flash()->error('Importance was not found!');
+
+            Redirect::to('/importances');
+        }
+
+        View::make('hospitals-edit.html', array('hospital' => $hospital));
+    }
+
+    public static function update($id)
+    {
+        $params   = $_POST;
+        $hospital = Hospital::find($id);
+
+        if ($hospital == null) {
+            flash()->error('Hospital was not found!');
+
+            Redirect::to('/hospitals');
+        }
+
+        $hospital->name = $params['name'];
+
+        $errors = $hospital->errors();
+        if (count($errors) > 0) {
+            flash()->error(':(', 'Something was a little off...');
+
+            Redirect::to('/hospitals/' . $hospital->id . '/edit', array('errors' => $errors, 'attributes' => $params));
+        }
+
+        $hospital->update();
+
+        flash('Hospital updated successfully!');
+        Redirect::to('/hospitals#' . $hospital->id);
+    }
+
+    public static function destroy($id)
+    {
+        $hospital = Hospital::find($id);
+
+        $hospital->destroy();
+
+        flash('Hospital removed successfully!');
+
+        Redirect::to('/hospitals');
+    }
+}
